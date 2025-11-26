7213c1bbda7aa23c760fb040f1e71a533cc7972d
Ritika Srivastava <158809116+RitikaSrivastava2204@users.noreply.github.com>
2025-11-27 04:06:10 +0530

    Time: 1 ms (91.14%), Space: 46.8 MB (37.52%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,6 +1,24 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int maxProduct = Integer.MIN_VALUE;
+        int productPrefix = 1;
+        int productSuffix = 1;
+        int last = nums.length-1;
+        for(int i=0; i<nums.length; i++){
+             if(productPrefix == 0){
+                productPrefix = 1;
+            }
+            if(productSuffix == 0){
+                productSuffix = 1;
+            }
+            productPrefix *= nums[i];
+            productSuffix *= nums[last-i];
+            maxProduct = Math.max(Math.max(productPrefix, productSuffix),maxProduct);
+        }
+        return maxProduct;
+    }
+    public int maxProductBruteForce(int[] nums) {
+        int maxProduct = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++){
             int product = 1;
             for(int j=i; j<nums.length; j++){
@@ -10,4 +28,5 @@ class Solution {
         }
         return maxProduct;
     }
+
 }
